eb1f6c7b2a8efd96636b090b02df83aefee91072
MAlngaawy <m.alngaawy@gmail.com>
2022-03-24 00:09:43 +0200

    try simple API for test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [content,setContent] = useState([]); 
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(json => setContent(json))
+      .then(console.log(content))
+  }, []);
+
   return (
     <div className="App">
-      learn react
+      This is the App
+      {content.map(post  => {
+        return (
+          <div key={post.id}>
+            <p className='title'>{post.title}</p>
+            <p className='body'>{post.body}</p>
+          </div>
+        )
+      })}
     </div>
   );
 }
