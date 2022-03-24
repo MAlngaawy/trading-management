2083f3caeb6ed36987a7509831166bf41460c82d
MAlngaawy <m.alngaawy@gmail.com>
2022-03-24 09:36:08 +0200

    fetch from strapi app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+  const [content, setContent] = useState("Test");
+  useEffect(() => {
+    console.log("Use Effect has work now ");
+    fetch("http://localhost:1337/api/test-single-type")
+      .then(res => res.json())
+      .then(data => setContent(data.data.attributes))
+      .then(console.log(content)); 
+  },[])
   return (
     <div className="App">
-      Test App
+      <h1>{content.name}</h1>
     </div>
   );
 }
