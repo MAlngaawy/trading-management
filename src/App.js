4cc35f6bd3e5f6f6b9cc33a4e52dd8a3c079971b
MAlngaawy <m.alngaawy@gmail.com>
2022-03-25 21:05:34 +0200

    arrange custmers in ordered list

--- src/App.js
+++ src/App.js
@@ -1,40 +1,36 @@
-import React, {useEffect, useState} from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
-  const [content,setContent] = useState([]); 
-  const [customers, setCustomers] = useState([]);
+    const [content, setContent] = useState([])
+    const [customers, setCustomers] = useState([])
 
-  // Use Effect to fetch data from the API
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(json => setContent(json))
-      .then(console.log(content))
-  }, []);
-  
+    // Use Effect to fetch data from the API
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => response.json())
+            .then((json) => setContent(json))
+            .then(console.log(content))
+    }, [])
 
-  // Use Effect to fetch data from my strapi API
-  useEffect(() => {
-    fetch('https://mo-strapi-playground.herokuapp.com/customers')
-    .then(res => res.json())
-    .then(json => setCustomers(json))
-    .then(console.log(customers))
-  },[])
+    // Use Effect to fetch data from my strapi API
+    useEffect(() => {
+        fetch('https://mo-strapi-playground.herokuapp.com/customers')
+            .then((res) => res.json())
+            .then((json) => setCustomers(json))
+            .then(console.log(customers))
+    }, [])
 
-  return ( 
-    <div className="App">
-      This is the App
-      {/* {content.map(post  => {
-        return (
-          <div key={post.id}>
-            <p className='title'>{post.title}</p>
-            <p className='body'>{post.body}</p>
-          </div>
-        )
-      })} */}
-    </div>
-  );
+    return (
+        <div className="App">
+            Fetch The customers Data
+            <ol>
+                {customers.map((customer) => {
+                    return <li>{customer.customer_name}</li>
+                })}
+            </ol>
+        </div>
+    )
 }
 
-export default App;
+export default App
