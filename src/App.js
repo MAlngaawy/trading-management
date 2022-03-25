a9e32de5abf5a944358e1c835466e08e592bca91
MAlngaawy <m.alngaawy@gmail.com>
2022-03-25 20:47:23 +0200

    fetch data from my API

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 
 function App() {
   const [content,setContent] = useState([]); 
+  const [customers, setCustomers] = useState([]);
 
+  // Use Effect to fetch data from the API
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
@@ -11,17 +13,25 @@ function App() {
       .then(console.log(content))
   }, []);
 
-  return (
+  // Use Effect to fetch data from my strapi API
+  useEffect(() => {
+    fetch('https://mo-strapi-playground.herokuapp.com/customers')
+    .then(res => res.json())
+    .then(json => setCustomers(json))
+    .then(console.log(customers))
+  },[])
+
+  return ( 
     <div className="App">
       This is the App
-      {content.map(post  => {
+      {/* {content.map(post  => {
         return (
           <div key={post.id}>
             <p className='title'>{post.title}</p>
             <p className='body'>{post.body}</p>
           </div>
         )
-      })}
+      })} */}
     </div>
   );
 }
