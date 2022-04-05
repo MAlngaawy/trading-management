7a69bb44b17dcd0ea3089fbe5c3ca66fd7d92609
MAlngaawy <m.alngaawy@gmail.com>
2022-04-05 20:53:45 +0200

    create a customer component

--- src/App.js
+++ src/App.js
@@ -16,124 +16,21 @@ const CUSTOMERS = gql`
 `
 
 function App() {
-    // const [data, setData] = useState([])
-    // const nameRef = useRef('Fill It')
-    // const debtRef = useRef('Num It')
-
     const [loading, error, data] = useQuery(CUSTOMERS)
 
-    useEffect(() => {
-        console.log(data)
-    }, [data])
+    // useEffect(() => {
+    //     console.log(data)
+    // }, [data])
 
-    // fetch Data unction
-    // const fetchData = () => {
-    //     console.log('fetchData')
-    //     fetch('http://localhost:1337/api/customers')
-    //         .then((res) => res.json())
-    //         .then(({ data }) => setData(data))
-    // }
+    // if (loading) return <p>Loading ....</p>
+    // if (error) return <p>error ....</p>
 
-    if (loading) return <p>Loading ....</p>
-    if (error) return <p>error ....</p>
-
-    if (data) {
-        console.log(data)
-    }
-
-    // // Delete customer functio
-    // const daleteCusto = (id) => {
-    //     console.log('deleteCusto')
-    //     fetch(`http://localhost:1337/api/customers/${id}`, {
-    //         method: 'DELETE',
-    //     }).then((res) => fetchData())
-    // }
-
-    // // Add Customer Function
-    // const addCusto = (name, debt) => {
-    //     console.log(name, debt)
-    //     fetch('http://localhost:1337/api/customers', {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //         },
-    //         body: JSON.stringify({
-    //             data: { name: name, debt: debt },
-    //         }),
-    //     })
-    //         .then(() => fetchData())
-    //         .then(() => {
-    //             nameRef.current.value = ''
-    //             debtRef.current.value = ''
-    //         })
+    // if (data) {
+    //     console.log(data)
     // }
 
     // The Returned Doms
-    return (
-        <div className="App">
-            {/* Buttom To Fetch Data */}
-            {/* <button
-                    onClick={() => fetchData()}
-                    className="p-4 bg-cyan-600 text-white cursor-pointer hover:scale-125 transform transition-all text-sm font-bold"
-                >
-                    Show Customers
-                </button> */}
-
-            {/* Inputs to POST data */}
-            {/* <div className="add">
-                    <input
-                        ref={nameRef}
-                        className="border-2 my-4"
-                        type="text"
-                        aria-label="name"
-                    />
-                    <input
-                        className="border-2"
-                        type="number"
-                        aria-label="number"
-                        ref={debtRef}
-                    />
-                    <button
-                        onClick={() =>
-                            addCusto(
-                                nameRef.current.value,
-                                debtRef.current.value
-                            )
-                        }
-                        className="bg-gray-500 p-4 my-4 rounded-xl text-white  "
-                    >
-                        add customer
-                    </button>
-                </div> */}
-
-            {/* Un Orderd List for showing Data */}
-            <ul>
-                {/* {data.length == 0
-                        ? 'Lol'
-                        : data.map(({ id, attributes }) => {
-                              return (
-                                  <li
-                                      key={id}
-                                      className="my-4 flex justify-around align-middle text-sm"
-                                  >
-                                      <p className="inline-block">
-                                          {attributes.name}
-                                      </p>
-                                      <p className="inline-block">
-                                          {attributes.debt}
-                                      </p>
-                                      <button
-                                          onClick={() => daleteCusto(id)}
-                                          className="p-1 h-14 bg-red-500 rounded-sm text-white cursor-pointer hover:scale-110 transform transition-all text-sm font-bold"
-                                      >
-                                          delete this customer
-                                      </button>
-                                  </li>
-                              )
-                          })} */}
-            </ul>
-        </div>
-    )
+    return <div className="App"> Lolo </div>
 }
 
 export default App
