2cd884814d43f1945a8c5e202a52d7a1d83247f8
MAlngaawy <m.alngaawy@gmail.com>
2022-04-10 12:35:07 +0200

    handle inputs and requests body

--- src/components/AddCustomer.jsx
+++ src/components/AddCustomer.jsx
@@ -1,37 +1,112 @@
 import React, { useState, useEffect } from 'react'
 
+// DOES GRAPHQL JUST GET DATA NOT POST DATA !!!!!
+
 const AddCustomer = () => {
-    const [name, setName] = useState('')
+    const [values, setvalues] = useState({
+        name: '',
+        debt: '',
+        phone: '',
+        address: '',
+    })
 
-    // useEffect((event) => {
-    //     setName()
-    // })
+    const handleChangeName = (e) => {
+        setvalues({
+            ...values,
+            name: e.target.value,
+        })
+    }
+    const handleChangedebt = (e) => {
+        setvalues({
+            ...values,
+            debt: e.target.value,
+        })
+    }
+    const handleChangephone = (e) => {
+        setvalues({
+            ...values,
+            phone: e.target.value,
+        })
+    }
 
-    const handleChange = (e) => {
-        setName(e.target.value)
+    const handleChangeaddress = (e) => {
+        setvalues({
+            ...values,
+            address: e.target.value,
+        })
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log('This is the submit button', name)
+        // console.log('This is the submit button', values)
+        // setvalues({
+        //     name: '',
+        //     debt: '',
+        //     phone: '',
+        //     address: '',
+        // })
+
+        fetch('http://localhost:1337/api/customers', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(values),
+        }).then((res) => console.log(res.json))
     }
 
     return (
-        <div className="addCustomer">
+        <div className="addCustomer text-lg">
+            {/* <p>
+                This is the input value
+                <span className="text-main mx-10">{values.toString()}</span>
+            </p> */}
             <form onSubmit={handleSubmit}>
-                <label>
-                    name
-                    <input
-                        type="text"
-                        value={name}
-                        onChange={handleChange}
-                        className="border-2 block"
-                    />
-                </label>
-                <input type="submit" name="submit" value="SUBMIT" />
+                <Inputs
+                    labelName="name"
+                    handleFunction={handleChangeName}
+                    value={values.name}
+                    type="text"
+                />
+                <Inputs
+                    labelName="debt"
+                    handleFunction={handleChangedebt}
+                    value={values.debt}
+                    type="number"
+                />
+                <Inputs
+                    labelName="phone"
+                    handleFunction={handleChangephone}
+                    value={values.phone}
+                    type="number"
+                />
+                <Inputs
+                    labelName="address"
+                    handleFunction={handleChangeaddress}
+                    value={values.address}
+                    type="text"
+                />
+                <input
+                    className="bg-main cursor-pointer text-white p-2 m-2"
+                    type="submit"
+                    name="submit"
+                    value="SUBMIT"
+                />
             </form>
         </div>
     )
 }
 
 export default AddCustomer
+
+const Inputs = ({ labelName, handleFunction, value, type }) => {
+    return (
+        <label>
+            {labelName}
+            <input
+                type={type}
+                value={value}
+                onChange={handleFunction}
+                className="border-2 block"
+            />
+        </label>
+    )
+}
