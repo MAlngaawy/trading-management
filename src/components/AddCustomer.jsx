7a3dbbff0ca85a0fd907805d6e0a0e9d39023aef
MAlngaawy <m.alngaawy@gmail.com>
2022-04-10 11:15:01 +0200

    initialize very simple form

--- src/components/AddCustomer.jsx
+++ src/components/AddCustomer.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const AddCustomer = () => {
-    return <h1>Add This Customer</h1>
+    const [name, setName] = useState('')
+
+    // useEffect((event) => {
+    //     setName()
+    // })
+
+    const handleChange = (e) => {
+        setName(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('This is the submit button', name)
+    }
+
+    return (
+        <div className="addCustomer">
+            <form onSubmit={handleSubmit}>
+                <label>
+                    name
+                    <input
+                        type="text"
+                        value={name}
+                        onChange={handleChange}
+                        className="border-2 block"
+                    />
+                </label>
+                <input type="submit" name="submit" value="SUBMIT" />
+            </form>
+        </div>
+    )
 }
 
 export default AddCustomer
