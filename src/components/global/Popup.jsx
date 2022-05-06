00d34cf730517310a119e99a45322aa70fe3ba54
MAlngaawy <m.alngaawy@gmail.com>
2022-05-06 22:44:58 +0200

    handle popup animation and size  option

--- src/components/global/Popup.jsx
+++ src/components/global/Popup.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import cn from 'classnames'
 
-const Popup = ({ children }) => {
+const Popup = ({ children, size }) => {
     return (
-        <div className="warning z-50 absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-black bg-opacity-50 w-full h-full">
-            <div className=" rounded-md warning-content w-4/5 h-4/5 max-h-80 max-w-md bg-white flex justify-center items-center flex-col">
+        <div className="z-50 absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-black bg-opacity-50 w-full h-full">
+            <div
+                className={cn(
+                    'animate-showUp rounded-md warning-content w-4/5 h-4/5 max-h-80 max-w-md bg-white flex justify-center items-center flex-col',
+                    { 'max-w-none max-h-max': (size = 'big') }
+                )}
+            >
                 {children}
             </div>
         </div>
